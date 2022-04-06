b130a968199d13ef8b02dc3f69948cfd293a5800
Kristjan Käärma <kris@kaarma.eu>
2022-04-06 11:50:03 +0300

    Updated template

--- resources/frontside-tailwind/src/App.vue
+++ resources/frontside-tailwind/src/App.vue
@@ -8,7 +8,7 @@
       <div class="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
         <div class="border-t border-gray-200 py-8 text-sm text-gray-500 text-center sm:text-left">
           <span class="block sm:inline left">&copy; {{ (new Date()).getFullYear() }} LaravelVueTemplate OÜ.</span>
-          <span class="block sm:inline">All rights reserved.</span>
+          <span class="block sm:inline" v-t="'All rights reserved.'" />
         </div>
       </div>
     </footer>
